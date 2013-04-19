f20efb02ddc1684d08cca2dad14c1d3d2281d03f
Rudolf Hornig <rudi@omnetpp.org>
2013-04-19 14:08:10 +0200

    updated to work with omnetpp 4.3

--- ui/com.simulte.configeditor/src/com/simulte/configeditor/fieldeditors/AbstractFieldEditor.java
+++ ui/com.simulte.configeditor/src/com/simulte/configeditor/fieldeditors/AbstractFieldEditor.java
@@ -7,22 +7,22 @@ package com.simulte.configeditor.fieldeditors;
 import org.eclipse.swt.events.FocusEvent;
 import org.eclipse.swt.events.FocusListener;
 import org.eclipse.swt.widgets.Control;
+import org.omnetpp.common.ui.HoverInfo;
 import org.omnetpp.common.ui.HoverSupport;
-import org.omnetpp.common.ui.IHoverTextProvider;
-import org.omnetpp.common.ui.SizeConstraint;
-
+import org.omnetpp.common.ui.HtmlHoverInfo;
+import org.omnetpp.common.ui.IHoverInfoProvider;
 import com.simulte.configeditor.propertysources.IPropertySource;
 
 /**
  * Abstract base class for field editors. See IFieldEditor for more info.
- *  
+ *
  * @author Andras
  */
 public abstract class AbstractFieldEditor implements IFieldEditor {
     protected boolean isInitialized = false;
     protected IPropertySource propertySource;
     protected HoverSupport hoverSupport;
-    
+
     public AbstractFieldEditor(IPropertySource propertySource, HoverSupport hoverSupport) {
         this.propertySource = propertySource;
         this.hoverSupport = hoverSupport;
@@ -30,14 +30,14 @@ public abstract class AbstractFieldEditor implements IFieldEditor {
 
     /**
      * Fill the control.
-     * 
-     * Note: may only be called when NedResources has been fully loaded! (see isLoadingInProgress()) 
+     *
+     * Note: may only be called when NedResources has been fully loaded! (see isLoadingInProgress())
      */
     public void initialize() {
         propertySource.initialize();
         isInitialized = true;
     }
-    
+
     @Override
     public boolean isInitialized() {
         return isInitialized;
@@ -47,14 +47,14 @@ public abstract class AbstractFieldEditor implements IFieldEditor {
     public IPropertySource getPropertySource() {
         return propertySource;
     }
-    
+
     @Override
     public void dispose() {
         // default implementation which is usually OK, but maybe not if some compound control is used
         getControlForLayouting().dispose();
         hoverSupport.forget(getControlForLayouting());
     }
-    
+
     /**
      * Utility function for subclasses
      */
@@ -68,15 +68,16 @@ public abstract class AbstractFieldEditor implements IFieldEditor {
             }
         });
     }
-    
+
     /**
      * Utility function for subclasses
      */
     protected void addHoverSupport(Control control) {
-        hoverSupport.adapt(control, new IHoverTextProvider() {
+        hoverSupport.adapt(control, new IHoverInfoProvider() {
+
             @Override
-            public String getHoverTextFor(Control control, int x, int y, SizeConstraint outSizeConstraint) {
-                return propertySource.getHoverText();
+            public HoverInfo getHoverFor(Control control, int x, int y) {
+                return new HtmlHoverInfo(propertySource.getHoverText());
             }
         });
     }
